c1a52e2c59e8133c2dc7ae585aa01d04a4ac8d38
ygtzz <ygtzz@126.com>
2016-08-25 17:34:46 +0800

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
-var TransferWebpackPlugin = require('transfer-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var extractCss = new ExtractTextPlugin('style/[name].css');
 
 module.exports = {
@@ -40,14 +41,17 @@ module.exports = {
             minChunks: 2
         }),
         new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
-        new TransferWebpackPlugin([
+        new CopyWebpackPlugin([
             { from: 'src/static',to:'/static'},
-            { from: 'node_modules/react/dist',to:'node_modules/react/dist'},                      
-        ],path.join(__dirname, '/')),
+            { from: 'node_modules/react/dist',to:'node_modules/react/dist'}
+        ]),
+        //clean folder
+        // new CleanWebpackPlugin(['dist'], {
+        //     verbose: true
+        // }),
         extractCss,        
         new webpack.ProvidePlugin({
-            // Automtically detect jQuery and $ as free var in modules and inject the jquery library
-            // This is required by many jquery plugins
+            // Automtically detect jQuery and $ as free var in modules and inject the jquery library,This is required by many jquery plugins
             $: "jquery",
             jQuery: "jquery",
             "window.jQuery": "jquery"
@@ -55,7 +59,18 @@ module.exports = {
 		new webpack.HotModuleReplacementPlugin(),        
         new webpack.NoErrorsPlugin()
 	],
+    eslint: {
+        configFile: '.eslintrc'
+    },
     module: {
+        preLoaders: [
+            //use eslint
+            // {
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     loader: 'eslint'
+            // }
+        ],
         loaders: [
             {test: /\.(js|jsx|es)$/, loader: "babel", exclude: /node_modules/},
             {test: /\.css$/, loader: extractCss.extract('style','css')},
--- webpack.prod.config..js
+++ webpack.prod.config..js
@@ -3,17 +3,69 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
-var TransferWebpackPlugin = require('transfer-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var extractCss = new ExtractTextPlugin('style/[name].css');
 
 module.exports = {
     entry: {
-        index: path.join(__dirname,'/src/pages/index/index.jsx')
+        index: './src/pages/index/index.jsx',
+        home: './src/pages/home/home.jsx'    
     },
     output: {
         path: './dist',
         filename: '[name].js',
         chunkFilename: "[chunkhash].js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+			template: './src/pages/index/index.html',
+            chunks: ['index'],
+            inject: 'body',
+            title: 'Index Page'
+		}),
+        new HtmlWebpackPlugin({
+            filename: 'home.html',
+			template: './src/pages/home/home.html',
+            chunks:['home'],
+            inject: 'body',
+            title: 'Home Page'
+		}),
+        new webpack.DefinePlugin({
+            __ENV__: JSON.stringify(process.env.NODE_ENV || 'dev')
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename:'common.js',
+            minChunks: 2
+        }),
+        new WebPack.optimize.UglifyJsPlugin({
+            compress:{
+                warnings: false
+            }
+        }),
+        new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+        new CopyWebpackPlugin([
+            { from: 'src/static',to:'/static'},
+            { from: 'node_modules/react/dist',to:'node_modules/react/dist'}
+        ]),
+        //clean folder
+        // new CleanWebpackPlugin(['dist'], {
+        //     verbose: true
+        // }),
+        extractCss,        
+        new webpack.ProvidePlugin({
+            // Automtically detect jQuery and $ as free var in modules and inject the jquery library,This is required by many jquery plugins
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),        
+		new webpack.HotModuleReplacementPlugin(),        
+        new webpack.NoErrorsPlugin()
+	],
+    eslint: {
+        configFile: '.eslintrc'
     },
     module: {
         loaders: [
@@ -31,31 +83,12 @@ module.exports = {
         'react': 'window.React',
         'jquery': 'window.jQuery'
     },
-    devtool: 'cheap-module-eval-source-map',//production use cheap-module-source-map    
+    //production use cheap-module-source-map 
+    devtool: 'cheap-module-source-map',   
     devServer:{
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-			template: './src/pages/index/index.html'
-		}),
-        new webpack.DefinePlugin({
-            __ENV__: JSON.stringify(process.env.NODE_ENV || 'dev')
-        }),
-        new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
-        new TransferWebpackPlugin([
-            { from: 'src/static',to:'/static'},
-            { from: 'node_modules/react/dist',to:'node_modules/react/dist'},                      
-        ],path.join(__dirname, '/')),
-        extractCss,        
-        new webpack.ProvidePlugin({
-            // Automtically detect jQuery and $ as free var in modules and inject the jquery library
-            // This is required by many jquery plugins
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        }),        
-		new webpack.HotModuleReplacementPlugin(),        
-        new webpack.NoErrorsPlugin()
-	]
+        contentBase: './dist',
+        hot: true,
+        inline: true,
+        progress: true
+    }
 };
