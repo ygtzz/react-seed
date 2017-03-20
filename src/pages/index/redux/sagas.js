22f663c7edb975aa6e45b3deb33c300945aeb4af
ygtzz <ygtzz@126.com>
2017-03-20 18:39:41 +0800

    add error handle

--- src/pages/index/redux/sagas.js
+++ src/pages/index/redux/sagas.js
@@ -6,18 +6,30 @@ import Promise from 'es6-promise';
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 function* fGetArticleDetailAsync(action) {
-    const articleId = action.payload.articleId;
-    const article = yield call(getArticleDetail,articleId);
-    yield put(actions.fGetArticleDetailOk(article));
+    try{
+        const articleId = action.payload.articleId;
+        const article = yield call(getArticleDetail,articleId);
+        yield put(actions.fGetArticleDetailOk(article));
+    }
+    catch(err){
+        //put error
+    }
+   
 }
 function* wacthArticleDetail(){
     yield takeEvery(actions.fGetArticleDetailStart,fGetArticleDetailAsync);
 }
 
 function* fGetCateListAsync(action) {
-    const {type,cate} = action.payload;
-    const aCate = yield call(getCateList,type,cate);
-    yield put(actions.fGetCateListOk(aCate));
+    try{
+        const {type,cate} = action.payload;
+        const aCate = yield call(getCateList,type,cate);
+        yield put(actions.fGetCateListOk(aCate));
+    }
+    catch(err){
+        //put err
+    }
+   
 }
 function* watchCateList(){
     let action = '';
@@ -27,18 +39,30 @@ function* watchCateList(){
 }
 
 function* fGetArticleListAsync(action) {
-    const {type,cate} = action.payload;
-    const aArticle = yield call(getArticleList,type,cate);
-    yield put(actions.fGetArticleListOk(aArticle));
+    try{
+        const {type,cate} = action.payload;
+        const aArticle = yield call(getArticleList,type,cate);
+        yield put(actions.fGetArticleListOk(aArticle));
+    }
+    catch(err){
+        //put err
+    }
+    
 }
 function* watchActicleList(){
     yield takeEvery(actions.fGetArticleListStart,fGetArticleListAsync)
 }
 
 function* fSearchArticlesAsync(action) {
-    const keyword = action.payload.keyword
-    const aArticle = yield call(searchArticles,keyword);
-    yield put(actions.fSearchArticlesOk(aArticle));
+    try{
+        const keyword = action.payload.keyword
+        const aArticle = yield call(searchArticles,keyword);
+        yield put(actions.fSearchArticlesOk(aArticle));
+    }
+    catch(err){
+        //put err
+    }
+    
 }
 function* watchSearchAtricles(){
     yield takeEvery(actions.fSearchArticlesStart,fSearchArticlesAsync)
